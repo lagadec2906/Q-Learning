f0adc2c91ee1bc938ebf0b7e83546e86cd300b56
BenLag2906 <lagadecben@gmail.com>
2023-02-08 11:42:13 +0100

    Update q-learning.py

--- q-learning.py
+++ q-learning.py
@@ -31,6 +31,10 @@ def generate_grid_and_adjacence():
 
     
     #We construct a random map
+    #O is index of possible wall to the left.
+    #1 is index of possible wall to the Top.
+    #2 is index of possible wall to the right.
+    #3 is index of possible wall to the bottom.
     for i in range (0,len(grid_barrier)):
         for j in range (0,len(grid_barrier[0])):
             for k in range (0,len(grid_barrier[0][0])):
@@ -47,48 +51,53 @@ def generate_grid_and_adjacence():
                     grid_barrier[i][j+1][1] = grid_barrier[i][j][k]
                 else :
                   print("nothing")
-                  
-    #we process adjacensy matrix                
+    
+    #All the points of the grid/maze are reshape with the following order
+    #  0 | 4 | 8  | 12
+    #  1 | 5 | 9  | 13
+    #  2 | 6 | 10 | 14
+    #  3 | 7 | 11 | 15
+    #we process Adjacency matrix: our matrix is symetrical ( non-oriented graph )                
     for i in range (0,len(grid_barrier)):
         for j in range (0,len(grid_barrier[0])):
              for k in range (0,len(grid_barrier[0][0])):
                 
-                if  ( (grid_barrier[i][j][0] == 0) & ( 4 * (i) + j -4 > 0 ) & (abs( 4* i + j - (4 * (i) + j -4) ) < 6 ) & ( i > 0) ) : # no line between i and i-1
-                    adjacence[4* i + j] [ 4 * (i) + j -4] = 1 # these 2 case are possibly linked
-                    adjacence[ 4 * (i) + j -4][4* i + j]  = 1 # these 2 case are possibly linked
+                if  ( (grid_barrier[i][j][0] == 0) & ( 4 * (i) + j -4 > 0 ) & (abs( 4* i + j - (4 * (i) + j -4) ) < 6 ) & ( i > 0) ) :
+                    adjacence[4* i + j] [ 4 * (i) + j -4] = 1 # these 2 case are linked
+                    adjacence[ 4 * (i) + j -4][4* i + j]  = 1 # these 2 case are linked
                 
-                if ( (grid_barrier[i][j][1] == 0) &  ( 4 * (i) + j -1 > 0 ) & (abs( 4* i + j - ( 4 * i-1 + j  )) < 6 ) & ( j > 0) ): # no line between i and j+1
-                    adjacence[4* i + j] [ 4 * (i) + j -1 ] = 1 # these 2 case are possibly linked
-                    adjacence[ 4 * i-1  + j ][4* i + j]  = 1 # these 2 case are possibly linked
+                if ( (grid_barrier[i][j][1] == 0) &  ( 4 * (i) + j -1 > 0 ) & (abs( 4* i + j - ( 4 * i-1 + j  )) < 6 ) & ( j > 0) ): 
+                    adjacence[4* i + j] [ 4 * (i) + j -1 ] = 1 # these 2 case are linked
+                    adjacence[ 4 * i-1  + j ][4* i + j]  = 1 # these 2 case are linked
                 
-                if ( (grid_barrier[i][j][2] == 0) & ( 4 * (i) + j + 4 < 16 ) & (abs( 4* i + j - ( 4 * (i) + j + 4 ) ) < 6 ) & ( j < 3 )) : # no line between i and i-1
-                    adjacence[4* i + j] [ 4 * (i) + j + 4] = 1 # these 2 case are possibly linked
-                    adjacence[ 4 * (i) + j + 4][4* i + j]  = 1 # these 2 case are possibly linked
+                if ( (grid_barrier[i][j][2] == 0) & ( 4 * (i) + j + 4 < 16 ) & (abs( 4* i + j - ( 4 * (i) + j + 4 ) ) < 6 ) & ( j < 3 )) : 
+                    adjacence[4* i + j] [ 4 * (i) + j + 4] = 1 # these 2 case are linked
+                    adjacence[ 4 * (i) + j + 4][4* i + j]  = 1 # these 2 case are linked
                 
-                if ( (grid_barrier[i][j][3] == 0) & ( 4 * i+1 + j < 16 ) & (abs( 4* i + j - ( 4 * (i) + j + 1 ) ) < 6  ) & (i < 3) ): # no line between i and i-1
-                    adjacence[4* i + j] [ 4 * i+1 + j] = 1 # these 2 case are possibly linked
-                    adjacence[ 4 *i + j + 1] [4* i + j]  = 1
+                if ( (grid_barrier[i][j][3] == 0) & ( 4 * i+1 + j < 16 ) & (abs( 4* i + j - ( 4 * (i) + j + 1 ) ) < 6  ) & (i < 3) ): 
+                    adjacence[4* i + j] [ 4 * i+1 + j] = 1 # these 2 case are linked
+                    adjacence[ 4 *i + j + 1] [4* i + j]  = 1 # these 2 case are linked
 
 
 
                 if ( (grid_barrier[i][j][0] == 1) & (grid_barrier[i][j][1] == 1) & ( 4 * i+ j -5 > 0 )  ) : # no line between i and i-1
-                    adjacence[4* i + j] [ 4 * i+ j -5] = 0 # these 2 case are possibly linked
-                    adjacence[ 4 * i+ j -5] [4* i + j]  = 0 # these 2 case are possibly linked
+                    adjacence[4* i + j] [ 4 * i+ j -5] = 0 # these 2 case are not linked
+                    adjacence[ 4 * i+ j -5] [4* i + j]  = 0 # these 2 case are not linked
                 
                     
                 if ( (grid_barrier[i][j][1] == 1) & (grid_barrier[i][j][2] == 1) & ( 4 * i + j + 3 < 16 ) ) : # no line between i and j+1
-                    adjacence[4* i + j] [ 4 * i + j + 3] = 0 # these 2 case are possibly linked
+                    adjacence[4* i + j] [ 4 * i + j + 3] = 0 # these 2 case are not linked
                     adjacence[ 4 * i + j + 3] [4* i + j]  = 0
                 
                     
 
                 if ( (grid_barrier[i][j][2] == 1) & (grid_barrier[i][j][3] == 1)  &  ( 4 * i + j + 5 < 16 ) ) : # no line between i and i-1
-                    adjacence[4* i + j] [ 4 * i + j + 5] = 0 # these 2 case are possibly linked
+                    adjacence[4* i + j] [ 4 * i + j + 5] = 0 # these 2 case are not linked
                     adjacence[ 4 * i + j + 5 ][4* i + j]  = 0
                 
                     
                 if ( (grid_barrier[i][j][3] == 1) & (grid_barrier[i][j][0] == 1) & ( 4 *i + j -3 > 0 )  ) : # no line between i and i-1
-                    adjacence[4* i + j] [ 4 *i + j -3 ] = 0 # these 2 case are possibly linked
+                    adjacence[4* i + j] [ 4 *i + j -3 ] = 0 # these 2 case are not linked
                     adjacence[4 *i + j -3 ][4* i + j]  = 0 
                 
 
@@ -97,29 +106,29 @@ def generate_grid_and_adjacence():
                  #diag sup right
                     if ( (4 * (i) + j +3 < 16) & (i < 3) & (j > 0) ):
                          if  ( ( (grid_barrier[i+1][j-1][0] == 1) & ( grid_barrier[i+1][j-1][3] == 1  ) ) | ( (grid_barrier[i][j][2] == 1) & ( grid_barrier[i][j][1] == 1  ) )  |  ( (grid_barrier[i][j][1] == 1) & ( grid_barrier[i+1][j][1] == 1  ) )  |  ( (grid_barrier[i][j][2] == 1) & ( grid_barrier[i+1][j-1][2] == 1  ) ) ):
-                            adjacence[4* i + j] [ 4 * i + j +3]  = 0 # these 2 case are possibly linked
-                            adjacence[ 4 * i + j +3] [4* i + j]  = 0 # these 2 case are possibly linked
+                            adjacence[4* i + j] [ 4 * i + j +3]  = 0 # these 2 case are not linked
+                            adjacence[ 4 * i + j +3] [4* i + j]  = 0 # these 2 case are not linked
 
                  #diag infer right
                     if ( 4 * i + j + 5 < 15  ) & (i < 3) & (j < 3) :
                         if  ( (grid_barrier[i][j][2] == 1) & (grid_barrier[i][j][3] == 1 ) ) | ( (grid_barrier[i][j][3] == 1) & (grid_barrier[i+1][j][3] == 1 ) ) | ( (grid_barrier[i][j][2] == 1) & (grid_barrier[i][j+1][2] == 1 )) | ( (grid_barrier[i+1][j+1][0] == 1) & (grid_barrier[i+1][j+1][1] == 1 ) ) :
-                            adjacence[4* i + j] [ 4 * i + j + 5]  = 0 # these 2 case are possibly linked
-                            adjacence[ 4 * i + j +5] [4* i + j]  = 0 # these 2 case are possibly linked
+                            adjacence[4* i + j] [ 4 * i + j + 5]  = 0 # these 2 case are not linked
+                            adjacence[ 4 * i + j +5] [4* i + j]  = 0 # these 2 case are not linked
 
                  #diag sup left
                     if ( (4 * (i) + j -5)  >  0 )  & (i > 0) & (j > 0) :
                          if  ( (grid_barrier[i][j][0] == 1) & (grid_barrier[i][j][1] == 1) ) | ( (grid_barrier[i-1][j-1][3] == 1) & (grid_barrier[i][j-1][3] == 1) )  | ( (grid_barrier[i][j][0] == 1) & (grid_barrier[i][j-1][0] == 1) ) |  ( (grid_barrier[i-1][j-1][3] == 1) & (grid_barrier[i-1][j-1][2] == 1) )  :
-                            adjacence[4* i + j] [ 4 * (i) + j -5]  = 0 # these 2 case are possibly linked
-                            adjacence[ 4 * (i) + j -5] [4* i + j]  = 0 # these 2 case are possibly linked
+                            adjacence[4* i + j] [ 4 * (i) + j -5]  = 0 # these 2 case are not linked
+                            adjacence[ 4 * (i) + j -5] [4* i + j]  = 0 # these 2 case are not linked
 
                  #diag inf left
                     if ( (4 * (i) + j -3)  >  0  ) & (i > 0) & (j  < 3):
                         if ( (grid_barrier[i][j][0] == 1) & (grid_barrier[i][j][3] == 1 ) ) | ( (grid_barrier[i][j][3] == 1) & (grid_barrier[i-1][j +1][3] == 1 ) ) | ( (grid_barrier[i][j][0] == 1) & (grid_barrier[i][j+1][0] == 1 ) ) | ( (grid_barrier[i-1][j+1][0] == 1) & (grid_barrier[i][j][3] == 1 )) | ( (grid_barrier[i][j][0] == 1) & (grid_barrier[i][j][3] == 1 ) ) :
-                            adjacence[4* i + j] [ 4 * (i-1) + j +1]  = 0 # these 2 case are possibly linked
-                            adjacence[ 4 * (i-1) + j +1] [4* i + j]  = 0 # these 2 case are possibly linked
+                            adjacence[4* i + j] [ 4 * (i-1) + j +1]  = 0 # these 2 case are not linked
+                            adjacence[ 4 * (i-1) + j +1] [4* i + j]  = 0 # these 2 case are not linked
     print("Display grid")            
     print(grid_barrier)
-    print("Display Agjacence")
+    print("Display Adjacence")
     print(adjacence) 
                         
 
@@ -129,8 +138,8 @@ The kpi process is number of path which could solve the problem.
 So we introduce basically the following "loss" ;
 more the grid is difficult : less the number of path to solve the issue is large.
 """
-def resolve_maze_process_kpi(start, end, maze_grid):
-    print("Resolve maze")
+def resolve_maze_process_kpi(start, end, treasure_grid):
+    print("Resolve maze and find treasure")
     maze = adjacence 
     puissance = adjacence
     # we process all the paths of different length
@@ -138,11 +147,11 @@ def resolve_maze_process_kpi(start, end, maze_grid):
         maze = maze + adjacence.dot(puissance)
         puissance = adjacence.dot(puissance)
     print(maze)   
-    # With a starting point a maze point and an exit point we process our KPI 'difficulty'
-    print("search" , maze[start][maze_grid])
-    print("exit" , maze[maze_grid][end] )
-    if ( (maze[start][maze_grid] > 0) & (maze[maze_grid][end] > 0 ) ) :
-        print("Loss/difficulty of generated grid" , maze[start][maze_grid]+maze[maze_grid][end] )
+    # With a starting point a treasure point and an exit point we process our KPI 'difficulty'
+    print("search" , maze[start][treasure_grid])
+    print("exit" , maze[treasure][end] )
+    if ( (maze[start][treasure] > 0) & (maze[treasure][end] > 0 ) ) :
+        print("Loss/difficulty of generated grid" , maze[start][treasure]+maze[treasure][end] )
     else :
         print("Impossible to solve")
 
